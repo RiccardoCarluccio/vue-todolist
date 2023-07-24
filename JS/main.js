833cf798f77e548a6069d41636df2abd5975a1c5
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-24 11:29:48 +0200

    array create

---
+++ JS/main.js
@@ -0,0 +1,35 @@
+"use strict"
+
+Vue.createApp({
+  data() {
+    return {
+      toDoList: [
+        {
+          id: 1,
+          text: alfa,
+          done: 1,
+        },
+        {
+          id: 2,
+          text: beta,
+          done: 0,
+        },
+        {
+          id: 3,
+          text: gamma,
+          done: 1,
+        },
+        {
+          id: 4,
+          text: delta,
+          done: 0,
+        },
+        {
+          id: 5,
+          text: epsilon,
+          done: 1,
+        }
+      ]
+    }
+  }
+})
